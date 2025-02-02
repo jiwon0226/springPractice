79c0ae30e707f71b3737312e8fdd0033eaad546e
jiwon0226 <ppg0226@naver.com>
2025-02-03 06:21:02 +0900

    feat: test 코드

--- board/src/main/java/com/example/demo/controller/HelloController.java
+++ board/src/main/java/com/example/demo/controller/HelloController.java
@@ -1,5 +1,12 @@
 package com.example.demo.controller;
 
-public class HelloController {
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+public class HelloController {
+    @RequestMapping("/")
+    public String hello() {
+        return "Hello World!!!";
+    }
 }
